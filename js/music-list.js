6ca1904292e530704d53b0e2d4e188229d8e2591
AJINKYA CHAVAN <87532290+AJINKYACHAVAN5@users.noreply.github.com>
2022-02-24 10:49:19 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "La Casa De Papel-Bella Ciao",
+    artist: "El-Professor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "CASABLENCA",
+    artist: "Saad-Lamjarred",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Isa",
+    artist: "Isa-Andro",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec-Benjamin",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "On My Way",
+    artist: "Alan Walker,Sabrina Carpenter & Farruko",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ciao Adios",
+    artist: "Anne-Marie",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Rockabye",
+    artist: "Sean Paul & Anne-Marie",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "I had a Dream",
+    artist: "Nina Nesbitt",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ola Ola",
+    artist: "Kate Linn",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Love Nwantiti",
+    artist: "Joeboy & kuami Eugene",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
